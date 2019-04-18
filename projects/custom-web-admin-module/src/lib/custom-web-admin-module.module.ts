96d01ec3af8ddd56eb25605306df941e1539dbbd
Marco Bonati <marco.bonati@vipera.com>
2019-04-18 21:20:11 +0200

    Added comboboxes

--- projects/custom-web-admin-module/src/lib/custom-web-admin-module.module.ts
+++ projects/custom-web-admin-module/src/lib/custom-web-admin-module.module.ts
@@ -1,15 +1,26 @@
 import { NgModule } from '@angular/core';
+import { CustomWebAdminModuleComponent } from './view/custom-web-admin-module.component';
 import { WCUIKitCoreModule,
   WCUIKitDataModule,
   WCUIKitKendoProviderModule,
   WCUIKitGridModule,
   WCUIKitDirectivesModule } from 'web-console-ui-kit';
-import { CustomWebAdminModuleComponent } from './view/custom-web-admin-module.component';
+import { GridModule } from '@progress/kendo-angular-grid';
+import { LoggerModule } from 'ngx-logger';
+import { CommonsUIModule, CommonSelectorsModule } from 'motif-web-admin-core';
 
 @NgModule({
   declarations: [CustomWebAdminModuleComponent],
   imports: [
-    WCUIKitCoreModule
+    WCUIKitCoreModule,
+    WCUIKitKendoProviderModule,
+    WCUIKitDataModule,
+    WCUIKitGridModule,
+    WCUIKitDirectivesModule,
+    GridModule,
+    LoggerModule,
+    CommonsUIModule,
+    CommonSelectorsModule
   ],
   exports: [CustomWebAdminModuleComponent]
 })
--- projects/custom-web-admin-module/src/lib/view/custom-web-admin-module.component.ts
+++ projects/custom-web-admin-module/src/lib/view/custom-web-admin-module.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, ViewChild } from '@angular/core';
 import { PluginView } from 'web-console-core';
+import { Domain, ApplicationsService, Application } from '@wa-motif-open-api/platform-service'
+import { ComboBoxComponent } from '@progress/kendo-angular-dropdowns';
+
 
 @Component({
   selector: 'cwa-custom-web-admin-module',
@@ -11,9 +14,38 @@ import { PluginView } from 'web-console-core';
 })
 export class CustomWebAdminModuleComponent implements OnInit {
 
+  @ViewChild('applicationsCombo') _appComboBox: ComboBoxComponent;
+  //@ViewChild('domainSelector') domainSelector: DomainSelectorComboBoxComponent;
+  //@ViewChild('applicationSelector') applicationSelector: ApplicationSelectorComboBoxComponent;
+
+  @Input() public selectedDomain: Domain;
+  public _selectedApplication: Application; // combo box selection
+
   constructor() { }
 
   ngOnInit() {
   }
+
+      /**
+    * Set the selcted application
+    */
+   @Input()
+   public set selectedApplication(application: Application) {
+       this._selectedApplication = application;
+       this.reloadData();
+   }
+
+   public get selectedApplication():Application {
+     return this._selectedApplication;
+   }
+
+   public onRefreshClicked(): void {
+    this.reloadData();
+   }
+
+  private reloadData(){
+    //TODO!!
+  }
+
 
 }
