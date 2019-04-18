3c8e926f819acfcfbf936cf8a45dc3b278cd46ec
Marco Bonati <marco.bonati@vipera.com>
2019-04-18 10:57:06 +0200

    Added web admin application

--- projects/custom-module-test-app/src/app/app.component.ts
+++ projects/custom-module-test-app/src/app/app.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { WCTopBarService, WCTopBarItem, AuthService, WCTopBarLocation } from 'web-console-core';
+import { TopMenuComponent, TopInfoComponent, TopLogoComponent } from 'motif-web-admin-core';
+import { WAThemeDesignerService } from 'motif-web-admin-core';
+import { HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'custom-module-test-app';
+
+
+  title = 'motif-web-admin-core-library-test';
+
+  constructor(private topBarService: WCTopBarService,
+              private authService: AuthService,
+              private themeDesignerService: WAThemeDesignerService){
+  }
+
+  ngOnInit() {
+    this.topBarService.registerItem(new WCTopBarItem('appInfo', TopInfoComponent), WCTopBarLocation.Left);
+    this.topBarService.registerItem(new WCTopBarItem('mainMenu', TopMenuComponent), WCTopBarLocation.Right);
+    this.topBarService.registerItem(new WCTopBarItem('logoTop', TopLogoComponent), WCTopBarLocation.Center);
+  }
+
+  ngOnDestroy() {
+  }
+
+  ngAfterContentInit() {
+  }
+
 }
